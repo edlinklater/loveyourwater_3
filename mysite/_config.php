0f7e4a216c3723f20fe4db8c449844ebda3a9fa7
Ben Manu <ben@silverstripe.com>
2014-11-29 10:12:34 +1300

    Changing config to use ss_environment

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,15 +3,20 @@
 global $project;
 $project = 'mysite';
 
-global $databaseConfig;
-$databaseConfig = array(
-	"type" => 'MySQLDatabase',
-	"server" => 'localhost',
-	"username" => 'loveyour_lyc',
-	"password" => 'jWl56ef0H82182R',
-	"database" => 'lyw3_db',
-	"path" => '',
-);
+global $database;
+$database = 'lyw3_db';
+
+require_once('conf/ConfigureFromEnv.php');
 
 // Set the site locale
 i18n::set_locale('en_US');
+
+// global $databaseConfig;
+// $databaseConfig = array(
+// 	"type" => 'MySQLDatabase',
+// 	"server" => 'localhost',
+// 	"username" => 'loveyour_lyc',
+// 	"password" => 'jWl56ef0H82182R',
+// 	"database" => 'lyw3_db',
+// 	"path" => '',
+// );
