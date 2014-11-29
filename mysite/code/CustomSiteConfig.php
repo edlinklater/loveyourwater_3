bb8ae320034525245126a6f98c3a58e1f455c9e7
Jesse Kershaw <Jesse.Kershaw@orionhealth.com>
2014-11-29 13:49:34 +1300

    Remove site config for terms

--- mysite/code/CustomSiteConfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-class CustomSiteConfig extends DataExtension {
-    private static $db = array(
-        'TermsAndConditions' => 'HTMLText'
-    );
-    public function updateCMSFields(FieldList $fields) {
-        $fields->addFieldToTab("Root.Main", new HTMLEditorField("TermsAndConditions", "Terms and Conditions"));
-    }
-}
