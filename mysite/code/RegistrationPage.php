43c02c05e6d15a581e2461baeb05ba0f9a79834b
Ben Manu <ben@silverstripe.com>
2014-11-29 15:30:51 +1300

    Adding email confirmation step

--- mysite/code/RegistrationPage.php
+++ mysite/code/RegistrationPage.php
@@ -2,7 +2,8 @@
 class RegistrationPage extends Page {
 
     private static $db = array(
-        'SuccessMessage' => 'HTMLText'
+        'SuccessMessage' => 'HTMLText',
+        'ConfirmedMessage' => 'HTMLText'
     );
 
     private static $has_one = array(
@@ -22,6 +23,8 @@ class RegistrationPage extends Page {
             ->setDescription('Page to link Terms and Conditions to.'));
         $fields->addFieldToTab('Root.Form', HTMLEditorField::create('SuccessMessage', 'Success Message')
             ->setDescription('Message to display after the form has been successfully submitted.'));
+        $fields->addFieldToTab('Root.Form', HTMLEditorField::create('ConfirmedMessage', 'Confirmed Message')
+            ->setDescription('Message to display after the member has successfully confirmed through their email.'));
 
         return $fields;
     }
@@ -31,7 +34,8 @@ class RegistrationPage extends Page {
 class RegistrationPage_Controller extends Page_Controller {
 
     private static $allowed_actions = array(
-        'Form'
+        'Form',
+        'verifyemail'
     );
 
     public function init() {
@@ -44,6 +48,51 @@ class RegistrationPage_Controller extends Page_Controller {
 
     public function isSuccess() {
         return $this->request->getVar('success');
+    }
+
+    public function isConfirmed() {
+        return $this->request->getVar('confirmed');
+    }
+
+    public function verifyemail() {
+        $code = $this->request->getVar('h');
+
+        $member = Member::get()->filter('VerificationCode', $code)->first();
+        if($member) {
+            $member->VerificationCode = null;
+            $member->VerificationExpiry = null;
+
+            if($member->PendingFormData) {
+                // save member data
+                $data = unserialize($member->PendingFormData);
+                $member->PendingFormData = null;
+                $member->IsVerified = true;
+                $member->write();
+
+                $member->update($data);
+                $member->write();
+
+                $member->changePassword($data['setPassword']);
+
+                if($this->GroupID) {
+                    $member->Groups()->add($this->Group());
+                }
+
+                // send email
+                $email = (new Email())
+                    ->setTo($member->Email)
+                    ->setSubject('Your Love Your Water account confirmation!')
+                    ->setTemplate('RegistrationConfirmationEmail')
+                    ->populateTemplate(array(
+                        'Member' => $member,
+                        'BaseURL' => Director::absoluteBaseURL()
+                    ))
+                    ->send();
+
+                // redirect
+                return $this->redirect($this->Link('?confirmed=1'));
+            }
+        }
     }
 
 }
--- mysite/code/forms/RegistrationForm.php
+++ mysite/code/forms/RegistrationForm.php
@@ -27,14 +27,56 @@ class RegistrationForm extends Form {
     }
 
     public function doRegistration($data, $form, $request) {
-        // create member
-        $member = new Member($data);
-        $member->write();
-        $member->changePassword($data['setPassword']['_Password']);
 
-        if($this->controller->GroupID) {
-            $member->Groups()->add($this->controller->Group());
+        $memberData = $data;
+
+        if(isset($memberData['setPassword']['_Password'])) {
+            $memberData['setPassword'] = $memberData['setPassword']['_Password'];
         }
+
+        $member = Member::get()->filter(array(
+            'Email' => $data['Email']
+        ))->first();
+
+        if($member) {
+            $form->sessionMessage("A member exists with the same email address!", 'bad');
+            return $this->controller->redirectBack();
+        }
+
+        $member = new Member();
+        $member->Email = $data['Email'];
+        $member->PendingFormData = serialize($memberData);
+        $member->write();
+
+        // set verification data
+        $generator = new RandomGenerator();
+        $code = $generator->randomToken();
+        $member->VerificationCode = $code;
+        $member->VerificationExpiry = date(
+            'Y-m-d H:i:s',
+            strtotime(
+                '+24 hours',
+                SS_Datetime::now()->Format('U')
+            )
+        );
+        $member->write();
+
+        // send email
+        $registrationPage = RegistrationPage::get()->first();
+        $link = $this->controller->join_links(
+            $registrationPage->Link('verifyemail'),
+            '?h=' . $code
+        );
+        $email = (new Email())
+            ->setTo($member->Email)
+            ->setSubject('Your Love Your Water account verification, nearly done!')
+            ->setTemplate('RegistrationVerificationEmail')
+            ->populateTemplate(array(
+                'Member' => $member,
+                'VerificationLink' => $link,
+                'BaseURL' => Director::absoluteBaseURL()
+            ))
+            ->send();
         
         // redirect to success page
         return $this->controller->redirect($this->controller->Link('?success=1'));
